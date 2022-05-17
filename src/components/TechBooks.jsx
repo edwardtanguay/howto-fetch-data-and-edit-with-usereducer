556675f011f0095ecd3940a96a4fd88259d7891b
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-17 06:33:10 +0200

    refactored to techBook

--- src/components/TechBooks.jsx
+++ src/components/TechBooks.jsx
@@ -1,21 +1,10 @@
+import { TechBook } from '../components/TechBook';
+
 export const TechBooks = ({ techBooks }) => {
 	return (
 		<div className="techBooks">
 			{techBooks.map((techBook, index) => {
-				return (
-					<div key={index} className="techBook">
-						<img
-							src={`https://edwardtanguay.netlify.app/share/images/techBooks/${techBook.idCode}.jpg`}
-							alt="tech book"
-						/>
-						<div className="info">
-							<div className="title">{techBook.title}</div>
-							<div className="description">
-								{techBook.description}
-							</div>
-						</div>
-					</div>
-				);
+				return <TechBook key={index} techBook={techBook} />;
 			})}
 		</div>
 	);
