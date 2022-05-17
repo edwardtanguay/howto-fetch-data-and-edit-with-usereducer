b5c45daba0c7cdc63f4705bb67f39cd8256e070c
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-17 07:03:15 +0200

    refactor to useReducer

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,21 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import { TechBooks } from '../components/TechBooks';
 
 const techBooksUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
 
+const reducer = (techBooks, action) => {
+	switch (action.type) {
+		case 'load':
+			return [...action.payload]
+	}
+};
+
 export const PageBooks = () => {
-	const [techBooks, setTechBooks] = useState([]);
+	const [techBooks, dispatch] = useReducer(reducer, []);
 
 	const handleDeleteTechBook = (techBook) => {
-		const _techBooks = techBooks.filter(m => m.id !== techBook.id);
-		setTechBooks(_techBooks);
+		// const _techBooks = techBooks.filter(m => m.id !== techBook.id);
+		// setTechBooks(_techBooks);
+		console.log('deleting techBook with id ' + techBook.id);
 	}
 
 	useEffect(() => {
 		(async () => {
 			const response = await fetch(techBooksUrl);
 			const _techBooks = await response.json();
-			setTechBooks(_techBooks);
+			// setTechBooks(_techBooks);
+			dispatch({ type: 'load', payload: _techBooks });
 		})();
 	}, []);
 
